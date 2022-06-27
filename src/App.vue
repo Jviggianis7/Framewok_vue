3497b7a8bb48704c8d8a308b268ff0a2d2c6531d
Jhon Machado <jhondviggianis@gmail.com>
2022-06-26 21:49:44 -0500

    actualización

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <mi-componente-1></mi-componente-1>
+  <miComponente2></miComponente2>
+  <mi-componente-3></mi-componente-3>
+</template>
+
+<script>
+import miComponente1 from './components/background.vue'
+import miComponente2 from './components/login.vue'
+import miComponente3 from './components/opciones.vue'
+
+export default {
+  name: 'App',
+  components: {
+    miComponente1,
+    miComponente2,
+    miComponente3
+  }
+}
+
+</script>
+
+<style lang='scss'>
+
+</style>
